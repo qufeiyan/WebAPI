140aaf3fc1974faca28b8d8c13aaa89f1ded75fe
Elena <2491411913@qq.com>
2017-12-07 02:00:02 +0800

    .net Web API test

---
+++ WebAPI/Controllers/UsersController1.cs
@@ -0,0 +1,83 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class UsersController1 : ApiController
+    {
+        // GET api/<controller>
+        public List<Users> Get()
+        {
+            List<Users> listUser = new List<Users>();
+            MySqlConnection mysql = getMySqlConnection();
+            MySqlCommand mySqlCommand = getSqlCommand("select * from user", mysql);
+            mysql.Open();
+            MySqlDataReader reader = mySqlCommand.ExecuteReader();
+            try
+            {
+                while (reader.Read())
+                {
+                    if (reader.HasRows)
+                    {
+                        Users user = new Users();
+                        user.UserID = reader.GetInt32("ID");
+                        user.UserName = reader.GetString("username");
+                        user.UserEmail = reader.GetString("useremail");
+                        listUser.Add(user);
+                    }
+                }
+            }
+            catch
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            finally
+            {
+                mysql.Close();
+            }
+            return listUser;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+
+        private static MySqlConnection getMySqlConnection()
+        {
+            MySqlConnection mysql = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString);
+            return mysql;
+
+        }
+        public static MySqlCommand getSqlCommand(String sql, MySqlConnection mysql)
+        {
+            MySqlCommand mySqlCommand = new MySqlCommand(sql, mysql);
+
+            return mySqlCommand;
+        }
+
+    }
+}
